5627d7a47e256116253848bcd966da041f39e33c
Ricard Sole <ricard.solecasas@gmail.com>
2014-08-06 14:55:43 +0200

    Updates

--- app/geo/showmap.js
+++
@@ -1,6 +0,0 @@
-function formatQuery(coordsArray){
-  var latitude = coordsArray[0];
-  var longitude = coordsArray[1];
-  var finalUrl = "http://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=16&size=640x640";
-  return finalUrl;
-}
